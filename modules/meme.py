939b4ab83bc456ba02997fc1defcf6bbd98c18a5
aryyyn <aryan.211618@ncit.edu.np>
2023-04-29 14:36:26 +0545

    finished meme.py

--- modules/meme.py
+++ modules/meme.py
@@ -1,8 +1,25 @@
 #aryn
 from discord.ext import commands
+import json
+import requests
+import discord
 
 @commands.command(name="meme")
 async def meme(ctx):
-    await ctx.channel.send("You are the meme hehehe!")
-    await ctx.channel.send("omg!")
+    memejson = json.loads(
+        requests.get("https://meme-api.com/gimme").text
+        )
 
+    
+    embed = discord.Embed(
+    title= memejson['title'],
+    url = memejson['postLink'],
+    )
+    embed.set_image(url=memejson['url'])
+    embed.set_footer(text=memejson['author'])
+    meme = await ctx.send(embed=embed)
+    await meme.add_reaction("👍")
+    await meme.add_reaction("👎")
+    
+
+   
