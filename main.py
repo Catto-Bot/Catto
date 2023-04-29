4466e0860a0b504ef703556dd6a5cdf7d228a4bd
aryyyn <aryan.211618@ncit.edu.np>
2023-04-29 12:56:35 +0545

    first comit

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
-    print("The bot is ready omg omg omg")
+    print("The bot is ready")
         
 @bot.event
 async def on_message(message):
