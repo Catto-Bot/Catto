81ceab13f2f01c80d23514b60a5ec233cc3854fd
kadotaislost <sachin2211068@iimscollege.edu.np>
2023-04-29 13:34:39 +0545

    added dice.py

--- modules/dice.py
+++ modules/dice.py
@@ -1,1 +1,11 @@
 #kadota
+import discord
+from discord.ext import commands
+import random
+
+
+@commands.command(name="rolldice")
+async def rolldice(ctx):
+    dice_roll = random.randint(1,6)
+    await ctx.channel.send(f"You rolled a {dice_roll}")
+
