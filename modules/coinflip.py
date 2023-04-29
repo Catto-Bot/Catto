0265c798cca0f42437fcd60a0b63f90877f6ba74
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2023-04-29 15:05:04 +0545

    coin

--- modules/coinflip.py
+++ modules/coinflip.py
@@ -1,11 +1,20 @@
 #nitrix
+import discord
 from discord.ext import commands
 import random
+import requests
 
-@commands.command(name="roll_dice")
-async def roll_dice(ctx):
-    """
-    Roll a six-sided die and display the result.
-    """
-    result = random.randint(1, 6)
-    await ctx.channel.send(f"The result is {result}!")
+# @commands.command(name="flip")
+# async def coin_flip(ctx):  
+#     list=["Heads","Tails"]
+#     result = random.choice(list)
+#     await ctx.channel.send(f" You got {result}")
+
+
+
+@commands.command(name="flip")
+async def coin_flip(ctx,member:commands.MemberConverter):  
+    list=["Heads","Tails"]
+    result1 = random.choice(list)
+    result2 = random.choice(list)
+    await ctx.channel.send(f" You got {result1} and {member.mention} got {result2}")
