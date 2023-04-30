6a2bf4371783e2dd7599d51c08d9ed824754e6db
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2023-04-30 22:11:16 +0545

    coinflip

--- modules/coinflip.py
+++ modules/coinflip.py
@@ -10,11 +10,14 @@ async def coin_flip(ctx, member: commands.MemberConverter = None):
     
     list = ["Heads", "Tails"]
     result1 = random.choice(list)
+
     if member:
         result2 = random.choice(list)
-        await ctx.channel.send(f"You got {result1} and {member.mention} got {result2}")
+        embed=discord.Embed(title="Coin Flip", description=f"You got {result1} and {member.mention} got {result2}",color=0x333333)
+        await ctx.send(embed=embed)
+
     else:
-        embed=discord.Embed(title=f"You got {result1}")
+        embed=discord.Embed(title="Coin Flip",description=f"You got {result1}",color=0x333333)
         await ctx.send(embed=embed)
 
 
