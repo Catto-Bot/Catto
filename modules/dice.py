e8e21fa0e20acc3de6b690f7d70315fd64923a11
aryyyn <aryan.211618@ncit.edu.np>
2023-04-30 21:30:46 +0545

    dice for kadota

--- modules/dice.py
+++ modules/dice.py
@@ -7,17 +7,18 @@ import random
 @commands.command(name="rolldice")
 async def rolldice(ctx, member: commands.MemberConverter = None):
     dice_roll = random.randint(1,6)
-    
+
     if member:
         dice_roll2 = random.randint(1,6)
         outcome_msg = f"{ctx.author.name} rolled a {dice_roll}! and {member.name} rolled a {dice_roll2}!"
         if dice_roll > dice_roll2:
-            outcome_msg += f" \n{ctx.author.name} WON!!!"
+            outcome_msg += f"\n {ctx.author.name} WON!!!"
         elif dice_roll2 > dice_roll:
-            outcome_msg += f" \n{member.name} WON!!!"
+            outcome_msg += f"\n {member.name} WON!!!"
         else:
-            outcome_msg += " \nDRAW!!"
-        await ctx.channel.send(outcome_msg)
-    else:
-        await ctx.channel.send(f"You rolled {dice_roll}!")
+            outcome_msg += " draw"
 
+        embed = discord.Embed(title=f"{ctx.author.name} VS {member.name}", description=outcome_msg, color=discord.Color.red())
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send(f"You rolled {dice_roll}!")
