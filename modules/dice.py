a4b7c8c756928db4e1e18c4e97e7f867251c419b
kadotaislost <sachin2211068@iimscollege.edu.np>
2023-04-30 21:16:21 +0545

    new dice function added

--- modules/dice.py
+++ modules/dice.py
@@ -10,14 +10,14 @@ async def rolldice(ctx, member: commands.MemberConverter = None):
     
     if member:
         dice_roll2 = random.randint(1,6)
-        await ctx.channel.send(f"{ctx.author.name} rolled a {dice_roll}! and {member.name}! rolled a {dice_roll2}")
-        if dice_roll>dice_roll2:
-            await ctx.channel.send(f"{ctx.author.name} WON!!!")
-        
-        elif dice_roll2>dice_roll:
-            await ctx.channel.send(f"{member.name} WON!!!")
+        outcome_msg = f"{ctx.author.name} rolled a {dice_roll}! and {member.name} rolled a {dice_roll2}!"
+        if dice_roll > dice_roll2:
+            outcome_msg += f" \n{ctx.author.name} WON!!!"
+        elif dice_roll2 > dice_roll:
+            outcome_msg += f" \n{member.name} WON!!!"
         else:
-            await ctx.channel.send("draw")
+            outcome_msg += " \nDRAW!!"
+        await ctx.channel.send(outcome_msg)
     else:
-        await ctx.channel.send(f"You rolled {dice_roll}")
+        await ctx.channel.send(f"You rolled {dice_roll}!")
 
