6b51c52414f1e3a59705a42632f82fc4e36e2e22
kadotaislost <sachin2211068@iimscollege.edu.np>
2023-04-30 21:02:05 +0545

    dice

--- modules/dice.py
+++ modules/dice.py
@@ -5,7 +5,19 @@ import random
 
 
 @commands.command(name="rolldice")
-async def rolldice(ctx):
+async def rolldice(ctx, member: commands.MemberConverter = None):
     dice_roll = random.randint(1,6)
-    await ctx.channel.send(f" bahahaha You rolled a {dice_roll}")
+    
+    if member:
+        dice_roll2 = random.randint(1,6)
+        await ctx.channel.send(f"{ctx.author.name} rolled a {dice_roll}! and {member.name}! rolled a {dice_roll2}")
+        if dice_roll>dice_roll2:
+            await ctx.channel.send(f"{ctx.author.name} WON!!!")
+        
+        elif dice_roll2>dice_roll:
+            await ctx.channel.send(f"{member.name} WON!!!")
+        else:
+            await ctx.channel.send("draw")
+    else:
+        await ctx.channel.send(f"You rolled {dice_roll}")
 
