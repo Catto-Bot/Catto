360686552fdcac02e5f076d124c04214f256a38f
aryyyn <aryan.211618@ncit.edu.np>
2023-05-04 22:29:39 +0545

    fixed some stuff for nitrix

--- modules/coinflip.py
+++ modules/coinflip.py
@@ -51,7 +51,7 @@ async def rps_game(ctx,user_choice):
 
 #announce
 
-@commands.command(name="announce")
+@commands.command(name="announce", aliases=["announcement"])
 @commands.has_permissions(administrator=True)
 async def announce(ctx,*,message:str):
 
@@ -69,17 +69,22 @@ async def announce(ctx,*,message:str):
         reaction, user = await ctx.bot.wait_for('reaction_add', timeout=20.0, check=check)
 
         if str(reaction.emoji) == "✅":
+            await announce_msg.delete()
             everyone= await ctx.send('@everyone')
             await everyone.delete()
             embed=discord.Embed(title="Announcement",description=f'{message}',color=0x333333)
             embed.set_footer(text="@everyone")
             await ctx.send(embed=embed)
+            return
         else:
+            await announce_msg.delete()
             await ctx.send(f'@everyone\n {message}')
+            return
     
-    except:
+    except Exception as err:
         no_response_del=await ctx.send("You did not respond in time")
         await no_response_del.delete(delay=5)
+        print(err)
 
 
 @announce.error
