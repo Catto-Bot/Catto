310720242ee1c99b77372f84177a7c332775d74c
aryyyn <aryan.211618@ncit.edu.np>
2023-05-18 19:21:34 +0545

    here

--- nsfw/animeNsfw.py
+++ nsfw/animeNsfw.py
@@ -12,7 +12,7 @@ def get_nsfw_channels(guild):
 
 urls = ['https://api.waifu.im/search/?included_tags=ass', 'https://api.waifu.im/search/?included_tags=hentai', 'https://api.waifu.im/search/?included_tags=milf', 'https://api.waifu.im/search/?included_tags=waifu&is_nsfw=true', 'https://api.waifu.im/search/?included_tags=oppai&is_nsfw=true', 'https://api.waifu.im/search/?included_tags=selfies&is_nsfw=true', 'https://api.waifu.im/search/?included_tags=uniform&is_nsfw=true']
 
-gifs = ['https://tenor.com/view/pervert-anime-gif-8409011','https://tenor.com/view/anime-kanna-you-perverts-pervert-gif-14346888', 'https://tenor.com/view/anime-yandere-creepy-peep-gif-20022951', 'https://tenor.com/view/caught-in-4k-caught-in4k-chungus-gif-19840038', 'https://tenor.com/view/anime-waiting-for-text-waiting-for-your-reply-gif-14108959','https://tenor.com/view/mirai-nikki-mao-mao-nonosaka-4k-caught-in4k-gif-23646198','https://tenor.com/view/kiniromozaic-kinmoza-pervert-you-pervert-mad-gif-5329720','https://tenor.com/view/kiniromozaic-kinmoza-pervert-you-pervert-mad-gif-5329720', 'https://tenor.com/view/anime-lewd-warning-nsfw-naughty-gif-8681415','https://tenor.com/view/nami-sanji-slap-one-piece-anime-gif-19985101']
+gifs = ['https://media.tenor.com/n1kNXs7w8q8AAAAd/pervert-anime.gif','https://media.tenor.com/t2pLAIENp_EAAAAC/anime-kanna.gif', 'https://media.tenor.com/BuA6tck7eTEAAAAC/anime-yandere.gif', 'https://media.tenor.com/QA6mPKs100UAAAAC/caught-in.gif', 'https://media.tenor.com/8yMqNs21uTQAAAAd/anime-waiting-for-text.gif','https://media.tenor.com/cKwZeGp3uMQAAAAC/mirai-nikki-mao.gif','https://media.tenor.com/JFUBQIDFTvcAAAAC/kiniromozaic-kinmoza.gif','https://media.tenor.com/JFUBQIDFTvcAAAAC/kiniromozaic-kinmoza.gif', 'https://media.tenor.com/6J2mC84fUtsAAAAC/anime-lewd.gif','https://media.tenor.com/00huTPPw5HIAAAAC/nami-sanji.gif']
 
 
 @commands.command()
@@ -27,14 +27,15 @@ async def hentai(ctx):
             return
         
         if ctx.channel.id != nsfw_channel.id:
-             await ctx.send('This is not a NSFW channel.😠')
-             await ctx.send(random.choice(gifs))
+             embed= discord.Embed(title="This is not a NSFW channel!!😠", description="")
+             embed.set_image(url=random.choice(gifs))
+             await ctx.send(embed=embed)
              return
         
         response = requests.get(random.choice(urls))
         data = response.json()
         url = data['images'][0]['url']
-        embed = discord.Embed(title='I am watching you pervert 🤨',color=0x333333)
+        embed = discord.Embed(title='',color=0x333333)
         embed.set_image(url = url)
 
         await nsfw_channel.send(embed = embed)
