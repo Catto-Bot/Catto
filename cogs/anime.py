2ceb2894b0bbb9732574671e4ec0c4ae57b3fe57
arynj0001 <unibixhi@gmail.com>
2026-06-01 13:17:24 +0545

    fix: lowercase animequote hybrid name

--- cogs/anime.py
+++ cogs/anime.py
@@ -9,7 +9,7 @@ class Anime(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.hybrid_command(name="animeQuote")
+    @commands.hybrid_command(name="animequote", aliases=["animeQuote"])
     async def anime_quote(self, ctx: commands.Context):
         log_command(ctx)
         loading = await ctx.send("loading…")
